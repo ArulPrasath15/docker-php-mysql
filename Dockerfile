f92f1819e643c6585c51616726d928e1d85b071d
Arul <arulprasath73@outlook.com>
2021-11-06 08:36:56 +0530

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM php:8.0-apache
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+
+WORKDIR /var/www/html
+
+COPY index.php .
+
+EXPOSE 80
---
+++ src/index.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Hello World!</h1>
+
+<?php
+
+getenv('MYSQL_HOST') ? $db_host=getenv('MYSQL_HOST') : $db_host="localhost";
+getenv('MYSQL_PORT') ? $db_port=getenv('MYSQL_PORT') : $db_port="3306";
+getenv('MYSQL_USER') ? $db_user=getenv('MYSQL_USER') : $db_user="root";
+getenv('MYSQL_PASS') ? $db_pass=getenv('MYSQL_PASS') : $db_pass="";
+getenv('MYSQL_NAME') ? $db_name=getenv('MYSQL_NAME') : $db_name="";
+
+if (strlen( $db_name ) === 0)
+  $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass);
+else 
+  $conn = new mysqli("$db_host:$db_port", $db_user, $db_pass, $db_name);
+
+// Check connection
+if ($conn -> connect_errno) {
+  ?>
+  <h1 style="color:red;" >Not Connected with MySQL</h1>
+
+<?php
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+else{
+  ?>
+  <h1 style="color:green;" >Connected with MySQL</h1>
+
+<?php
+}
+?>
+
+// Check connection
+if ($conn->connect_error) 
+	die("Connection failed: " . $conn->connect_error);
+ 
+if (!($result=mysqli_query($conn,'SHOW DATABASES')))
+    printf("Error: %s\n", mysqli_error($conn));
+
+echo "<h3>Databases</h3>";
+
+while($row = mysqli_fetch_row( $result ))
+    echo $row[0]."<br />";
+
+$result -> free_result();
+$conn->close();
+
+
+
+
+</body>
+</html>
